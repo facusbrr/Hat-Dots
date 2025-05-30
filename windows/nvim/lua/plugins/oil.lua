915d50a6634a2e20ef03216170e9a45fe70d7bcc
Facundo Gayoso <facusbrr@gmail.com>
2025-05-30 10:40:15 -0300

    fix: update a keymaps

--- windows/nvim/lua/plugins/oil.lua
+++ windows/nvim/lua/plugins/oil.lua
@@ -11,26 +11,34 @@ return {
       show_hidden = true,
     },
     keymaps = {
-      ["g?"] = "actions.show_help",
-      ["<CR>"] = "actions.select",
-      ["<C-s>"] = "actions.select_vsplit",
-      ["<C-h>"] = "actions.select_split",
-      ["<C-p>"] = "actions.preview",
-      ["<C-c>"] = "actions.close",
-      ["q"] = "actions.close",
-      ["<C-r>"] = "actions.refresh",
-      ["-"] = "actions.parent", -- ir a la carpeta anterior
+      ["g?"] = "actions.show_help", -- Show help
+      ["<CR>"] = "actions.select", -- Select entry
+      ["<C-M-s>"] = { "actions.select", opts = { vertical = true }, desc = "Open the entry in a vertical split" }, -- Open entry in vertical split
+      ["<C-d>"] = { "actions.select", opts = { horizontal = true }, desc = "Open the entry in a horizontal split" }, -- Open entry in horizontal split
+      ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" }, -- Open entry in new tab
+      ["<C-p>"] = "actions.preview", -- Preview entry
+      ["<C-c>"] = "actions.close", -- Close oil.nvim
+      ["<C-l>"] = "actions.refresh", -- Refresh oil.nvim
+      ["-"] = "actions.parent", -- Go to parent directory
+      ["_"] = "actions.open_cwd", -- Open current working directory
+      ["`"] = "actions.cd", -- Change directory
+      ["~"] = { "actions.cd", opts = { scope = "tab" }, desc = ":tcd to the current oil directory" }, -- Change directory for the current tab
+      ["gs"] = "actions.change_sort", -- Change sorting method
+      ["gx"] = "actions.open_external", -- Open entry with external application
+      ["g."] = "actions.toggle_hidden", -- Toggle hidden files
+      ["g\\"] = "actions.toggle_trash", -- Toggle trash
     },
-    use_default_keymaps = false,
+    use_default_keymaps = false, -- Do not use default key mappings
   },
   keys = {
     -- Atajo para abrir Oil en el directorio actual
     {
       "-", -- Suele usarse como atajo común
-      function() require("oil").open() end,
+      function()
+        require("oil").open()
+      end,
       desc = "Abrir Oil (explorador rápido)",
     },
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
-
